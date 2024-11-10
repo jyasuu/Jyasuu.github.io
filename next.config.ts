78c8be808216f041e86239f3eaf7b784533e1743
jyasuu <s1002605@pu.edu.tw>
2024-11-10 09:29:29 +0000

    _next > next

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export'
+  output: 'export',
+  distDir: 'build',
+  assetPrefix: '/next'
 };
 
 export default nextConfig;
