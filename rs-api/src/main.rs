466edbd7863c51cd3d42c7530ba02391e54888bc
jyasuu <s1002605@pu.edu.tw>
2024-12-02 13:04:46 +0000

    try middleware

--- rs-api/src/main.rs
+++ rs-api/src/main.rs
@@ -3,12 +3,76 @@ use actix_web::{
 };
 use actix_cors::Cors;
 use env_logger::Env;
-use std::env;
+use std::{env, future::Future, pin::Pin};
 
 mod models;
 mod handlers;
 mod errors;
 mod services;
+
+use std::future::{ready, Ready};
+
+use actix_web::{
+    dev::{forward_ready, Service, Transform},
+    Error,
+};
+
+// There are two steps in middleware processing.
+// 1. Middleware initialization, middleware factory gets called with
+//    next service in chain as parameter.
+// 2. Middleware's call method gets called with normal request.
+pub struct SayHi;
+
+// Middleware factory is `Transform` trait
+// `S` - type of the next service
+// `B` - type of response's body
+impl<S, B> Transform<S, ServiceRequest> for SayHi
+where
+    S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+    S::Future: 'static,
+    B: 'static,
+{
+    type Response = ServiceResponse<B>;
+    type Error = Error;
+    type InitError = ();
+    type Transform = SayHiMiddleware<S>;
+    type Future = Ready<Result<Self::Transform, Self::InitError>>;
+
+    fn new_transform(&self, service: S) -> Self::Future {
+        ready(Ok(SayHiMiddleware { service }))
+    }
+}
+
+pub struct SayHiMiddleware<S> {
+    service: S,
+}
+type LocalBoxFuture<T> = Pin<Box<dyn Future<Output = T> + 'static>>;
+
+impl<S, B> Service<ServiceRequest> for SayHiMiddleware<S>
+where
+    S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+    S::Future: 'static,
+    B: 'static,
+{
+    type Response = ServiceResponse<B>;
+    type Error = Error;
+    type Future = LocalBoxFuture<Result<Self::Response, Self::Error>>;
+
+    forward_ready!(service);
+
+    fn call(&self, req: ServiceRequest) -> Self::Future {
+        println!("Hi from start. You requested: {}", req.path());
+
+        let fut = self.service.call(req);
+
+        Box::pin(async move {
+            let res = fut.await?;
+
+            println!("Hi from response");
+            Ok(res)
+        })
+    }
+}
 
 
 async fn rate_limit(
@@ -54,6 +118,7 @@ async fn main() -> std::io::Result<()> {
             .wrap(Logger::default())
             .wrap(cors)
             .wrap(from_fn(rate_limit))
+            .wrap(SayHi)
             .service(
                 web::scope("/api")
                     .route("/chat/ollama", web::post().to(handlers::ollama_chat_handler))
